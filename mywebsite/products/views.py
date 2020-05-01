3b87d086b1e0c5800ded3b367cfd7ab5dd211c37
Adil HOUB <adil.houb@gmail.com>
2020-05-01 11:41:24 +0000

    test commit

--- mywebsite/products/views.py
+++ mywebsite/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello world')
+
+
+def newproduct(request):
+    return HttpResponse('This is a new product')
