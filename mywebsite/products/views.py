aec15b0b64ebaa5be0502dbcf0b73761cf7edee7
Adil HOUB <adil.houb@gmail.com>
2020-05-01 15:12:16 +0000

    the final commit after finishing the django course

--- mywebsite/products/views.py
+++ mywebsite/products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'products': products})
 
 
 def newproduct(request):
